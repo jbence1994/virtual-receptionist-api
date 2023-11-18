4288656d956afc306a9e63876a98e3f6b2b8876a
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-11-18 12:03:12 +0100

    Add Address data model.

---
+++ VirtualReceptionist/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class Address
+{
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string HouseNumber { get; set; }
+}
