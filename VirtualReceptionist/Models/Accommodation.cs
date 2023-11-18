c82fac91a9ce90a19e18efd5d419eb4e4c7e7e56
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-11-18 11:53:26 +0100

    Define Accommodation model.

---
+++ VirtualReceptionist/Models/Accommodation.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class Accommodation
+{
+    public string NtakId { get; set; }
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public string VatNumber { get; set; }
+    public string Contact { get; set; }
+    public string Headquarters { get; set; }
+    public string Site { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
