adc488e552df7af45772593232728c9b48ee7008
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2023-11-18 12:21:43 +0100

    Define data models (#3)

---
+++ VirtualReceptionist/Models/Guest.cs
@@ -0,0 +1,17 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class Guest
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string DocumentId { get; set; }
+    public Country Country { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public Address Address { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
---
+++ VirtualReceptionist/Models/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public Guest Guest { get; set; }
+    public Room Room { get; set; }
+    public int HeadCount { get; set; }
+    public DateOnly ArrivalDate { get; set; }
+
+    public DateOnly DepartureDate => ArrivalDate.AddDays(NumberOfNights);
+
+    public int NumberOfNights { get; set; }
+    public bool IsPaid { get; set; }
+}
