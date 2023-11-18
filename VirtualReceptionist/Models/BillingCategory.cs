3b73af1536add1f5ec4d50bbdb54b82c6e0183ac
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-11-18 12:08:57 +0100

    Add BillingCategory data model.

---
+++ VirtualReceptionist/Models/BillingCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class BillingCategory
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Vat { get; set; }
+    public string Unit { get; set; }
+}
