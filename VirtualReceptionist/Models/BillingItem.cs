52f8f0a9b33a7ce9cee01f694c7ec64351afa601
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-11-18 12:09:06 +0100

    Add BillingItem data model.

---
+++ VirtualReceptionist/Models/BillingItem.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtualReceptionist.Models;
+
+public class BillingItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public BillingCategory Category { get; set; }
+}
